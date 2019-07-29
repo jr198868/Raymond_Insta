cab068219f4b93f923e337880fd38a5687ae4ef5
Raymond <rjingraymond88@gmail.com>
2019-07-29 18:41:19 -0400

    Almost done

--- Insta/views.py
+++ Insta/views.py
@@ -5,8 +5,9 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from Insta.forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from annoying.decorators import ajax_request
+from Insta.models import Post, InstaUser, Like
 
-from Insta.models import Post
 
 
 # Create your views here.
@@ -41,3 +42,30 @@ class SignupView(CreateView):
     form_class = CustomUserCreationForm
     template_name = "signup.html"
     success_url = reverse_lazy('login')
+
+class UserDetail(DetailView):
+    model = InstaUser
+    template_name = "user_profile.html"
+
+class EditProfile(UpdateView):
+    model = InstaUser
+    template_name = "edit_profile.html"
+    fields = ('username','profile_pic',)
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
